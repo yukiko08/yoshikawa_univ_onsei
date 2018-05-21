2e03d0a8c320fa677ca906aabab9b3b423afb0d2
S2-yoshikawa <1514388t@stu.kobe-u.ac.jp>
2018-05-21 14:36:37 +0900

    first

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.comboBox_b = new System.Windows.Forms.ComboBox();
+            this.comboBox_s = new System.Windows.Forms.ComboBox();
+            this.go = new System.Windows.Forms.Button();
+            this.strings = new System.Windows.Forms.TextBox();
+            this.string_in = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bol_b = new System.Windows.Forms.TrackBar();
+            this.vol_s = new System.Windows.Forms.TrackBar();
+            this.speed_s = new System.Windows.Forms.TrackBar();
+            this.speed_b = new System.Windows.Forms.TrackBar();
+            this.Auto = new System.Windows.Forms.Button();
+            this.crrect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.itemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bol_b)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vol_s)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speed_s)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speed_b)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "一音再生";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(158, 207);
+            this.trackBar1.Minimum = -10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(260, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // comboBox_b
+            // 
+            this.comboBox_b.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox_b.FormattingEnabled = true;
+            this.comboBox_b.Location = new System.Drawing.Point(297, 55);
+            this.comboBox_b.Name = "comboBox_b";
+            this.comboBox_b.Size = new System.Drawing.Size(121, 43);
+            this.comboBox_b.TabIndex = 4;
+            this.comboBox_b.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox_s
+            // 
+            this.comboBox_s.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox_s.FormattingEnabled = true;
+            this.comboBox_s.Location = new System.Drawing.Point(158, 55);
+            this.comboBox_s.Name = "comboBox_s";
+            this.comboBox_s.Size = new System.Drawing.Size(121, 43);
+            this.comboBox_s.TabIndex = 5;
+            this.comboBox_s.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // go
+            // 
+            this.go.Location = new System.Drawing.Point(392, 258);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(26, 45);
+            this.go.TabIndex = 7;
+            this.go.Text = "go";
+            this.go.UseVisualStyleBackColor = true;
+            this.go.Click += new System.EventHandler(this.go_Click);
+            // 
+            // strings
+            // 
+            this.strings.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.strings.Location = new System.Drawing.Point(194, 272);
+            this.strings.Name = "strings";
+            this.strings.Size = new System.Drawing.Size(192, 31);
+            this.strings.TabIndex = 9;
+            // 
+            // string_in
+            // 
+            this.string_in.Location = new System.Drawing.Point(158, 260);
+            this.string_in.Name = "string_in";
+            this.string_in.Size = new System.Drawing.Size(30, 43);
+            this.string_in.TabIndex = 10;
+            this.string_in.Text = "in";
+            this.string_in.UseVisualStyleBackColor = true;
+            this.string_in.Click += new System.EventHandler(this.string_in_Click);
+            // 
+            // del
+            // 
+            this.del.Location = new System.Drawing.Point(248, 243);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(75, 23);
+            this.del.TabIndex = 11;
+            this.del.Text = "delete";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(312, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 29);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "母音";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(168, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 29);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "子音";
+            // 
+            // bol_b
+            // 
+            this.bol_b.Location = new System.Drawing.Point(438, 45);
+            this.bol_b.Maximum = 1000;
+            this.bol_b.Minimum = 100;
+            this.bol_b.Name = "bol_b";
+            this.bol_b.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.bol_b.Size = new System.Drawing.Size(45, 104);
+            this.bol_b.SmallChange = 10;
+            this.bol_b.TabIndex = 15;
+            this.bol_b.Value = 100;
+            // 
+            // vol_s
+            // 
+            this.vol_s.Location = new System.Drawing.Point(107, 45);
+            this.vol_s.Maximum = 1000;
+            this.vol_s.Minimum = 100;
+            this.vol_s.Name = "vol_s";
+            this.vol_s.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.vol_s.Size = new System.Drawing.Size(45, 104);
+            this.vol_s.SmallChange = 10;
+            this.vol_s.TabIndex = 16;
+            this.vol_s.Value = 100;
+            // 
+            // speed_s
+            // 
+            this.speed_s.Location = new System.Drawing.Point(158, 107);
+            this.speed_s.Maximum = 20;
+            this.speed_s.Name = "speed_s";
+            this.speed_s.Size = new System.Drawing.Size(104, 45);
+            this.speed_s.TabIndex = 17;
+            // 
+            // speed_b
+            // 
+            this.speed_b.Location = new System.Drawing.Point(297, 104);
+            this.speed_b.Maximum = 20;
+            this.speed_b.Name = "speed_b";
+            this.speed_b.Size = new System.Drawing.Size(104, 45);
+            this.speed_b.TabIndex = 18;
+            // 
+            // Auto
+            // 
+            this.Auto.Location = new System.Drawing.Point(98, 207);
+            this.Auto.Name = "Auto";
+            this.Auto.Size = new System.Drawing.Size(54, 23);
+            this.Auto.TabIndex = 19;
+            this.Auto.Text = "Auto→";
+            this.Auto.UseVisualStyleBackColor = true;
+            this.Auto.Click += new System.EventHandler(this.Auto_Click);
+            // 
+            // crrect
+            // 
+            this.crrect.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.crrect.Location = new System.Drawing.Point(547, 55);
+            this.crrect.Name = "crrect";
+            this.crrect.Size = new System.Drawing.Size(49, 54);
+            this.crrect.TabIndex = 20;
+            this.crrect.Text = "〇";
+            this.crrect.UseVisualStyleBackColor = true;
+            this.crrect.Click += new System.EventHandler(this.crrect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.itemBindingSource, "times", true));
+            this.label3.Location = new System.Drawing.Point(280, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "0";
+            // 
+            // itemBindingSource
+            // 
+            this.itemBindingSource.DataSource = typeof(WindowsFormsApplication1.Item);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 314);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.crrect);
+            this.Controls.Add(this.Auto);
+            this.Controls.Add(this.speed_b);
+            this.Controls.Add(this.speed_s);
+            this.Controls.Add(this.vol_s);
+            this.Controls.Add(this.bol_b);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.string_in);
+            this.Controls.Add(this.strings);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.comboBox_s);
+            this.Controls.Add(this.comboBox_b);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bol_b)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vol_s)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speed_s)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speed_b)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ComboBox comboBox_b;
+        private System.Windows.Forms.ComboBox comboBox_s;
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.TextBox strings;
+        private System.Windows.Forms.Button string_in;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar bol_b;
+        private System.Windows.Forms.TrackBar vol_s;
+        private System.Windows.Forms.TrackBar speed_s;
+        private System.Windows.Forms.TrackBar speed_b;
+        private System.Windows.Forms.Button Auto;
+        private System.Windows.Forms.Button crrect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.BindingSource itemBindingSource;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.DirectX.AudioVideoPlayback;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+   
+
+    public partial class Form1 : Form
+    {
+
+        CmbObject boin;
+        CmbObject shin;
+
+        Audio sound_b;
+        Audio sound_si;
+
+        int time;
+        Item item = new Item(0) ;
+        //time = item.Times;
+        
+        int volume;
+
+        string[] sounds = new string[] {"C:\\Users\\S2\\Music\\日本語発音\\wav\\none.wav"};
+
+        public Form1()
+        {
+            InitializeComponent();
+            strings.Text = " ";
+
+            //母音リスト
+
+
+            comboBox_b.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\none.wav", "×"));
+            comboBox_b.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\a.wav", "a"));
+            comboBox_b.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\i.wav", "i"));
+            comboBox_b.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\j.wav", "j"));
+            comboBox_b.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\u.wav", "u"));
+            comboBox_b.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\e.wav", "e"));
+            comboBox_b.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\o.wav", "o"));
+
+
+            
+            
+            //子音リスト
+          
+           
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\none.wav", "×"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\k.wav", "k"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\s.wav", "s"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\s_1.wav", "s_1"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\s_2.wav", "s_2"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\θ.wav", "θ"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\ʃ.wav", "ʃ"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\t.wav", "t"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\ʒ.wav", "ʒ"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\tʃ.wav", "tʃ"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\n.wav", "n"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\ŋ.wav", "ŋ"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\h.wav", "h"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\f.wav", "f"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\m.wav", "m"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\j.wav", "y"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\r.wav", "r"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\l.wav", "l"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\w.wav", "w"));
+
+            //濁点
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\g.wav", "g"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\z.wav", "z"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\d.wav", "d"));
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\dʒ.wav", "dʒ"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\b.wav", "b"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\p.wav", "p"));
+
+            comboBox_s.Items.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\v.wav", "v"));
+
+            
+
+        }
+
+        class CmbObject
+        {
+            public string Url { get; set; }
+            public string Value { get; set; }
+            public CmbObject(string Url, string Value)
+            {
+                this.Url = Url;
+                this.Value = Value;
+            }
+            public override string ToString()
+            {
+                return Value;
+            } 
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            sound_b = new Audio(boin.Url);
+            sound_si = new Audio(shin.Url);
+
+            if (shin.Value != null && boin.Value != null)
+            {
+                if (time > 0)
+                {
+                    sound_si.Play();
+                    System.Threading.Thread.Sleep(time * 10);
+                    sound_b.Play();
+                }
+                else
+                {
+                    sound_b.Play();
+                    System.Threading.Thread.Sleep(time * -10);
+                    sound_si.Play();
+                }
+            }
+            
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            boin = (CmbObject)comboBox_b.SelectedItem;
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            shin = (CmbObject)comboBox_s.SelectedItem;
+            
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            time = trackBar1.Value;
+            label3.Text = time.ToString();
+
+        }
+
+        private void string_in_Click(object sender, EventArgs e)
+        {
+            strings.AppendText(shin.Value);
+            strings.AppendText(boin.Value);
+            
+            Array.Resize(ref sounds, sounds.Length +2);
+            sounds[sounds.Length - 2] = shin.Url;
+            sounds[sounds.Length - 1] = boin.Url;
+        }
+
+        private void go_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < sounds.Length-1; i=i+2)
+            {
+                sound_si = new Audio(sounds[i]);
+                sound_b = new Audio(sounds[i+1]);
+
+
+                if (time > 0)
+                {
+                    sound_si.Play();
+                    System.Threading.Thread.Sleep(time * 10);
+                    sound_b.Play();
+                }
+                else
+                {
+                    sound_b.Play();
+                    System.Threading.Thread.Sleep(time * -10);
+                    sound_si.Play();
+                }
+                System.Threading.Thread.Sleep(500);
+
+            }
+            
+ 
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            strings.Text = " ";
+            Array.Resize(ref sounds, 1);
+            sounds[0] = "C:\\Users\\S2\\Music\\日本語発音\\wav\\none.wav";
+        }
+
+        private void Auto_Click(object sender, EventArgs e)
+        {
+            time = -10;
+            
+            while (time<=10)
+            {
+                trackBar1.Value = time;
+
+                label3.Text = time.ToString();
+                Console.WriteLine(time.ToString());
+
+                button1.PerformClick();
+                System.Threading.Thread.Sleep(800);
+                time++;
+
+            }
+
+        }
+
+        private void crrect_Click(object sender, EventArgs e)
+        {
+            //ここを追記処理にして，最後に書き込むようにする
+            var data = new List<List<string>>()
+            {
+                new List<string>(){shin.Value,boin.Value,time.ToString()},
+            };
+            using (var writer = new CsvWriter(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\result.csv"))
+            {
+                writer.Write(data);
+            }
+        }
+
+
+      
+
+        
+    }
+    public class Item
+    {
+        public int Times { get; set; }
+        public Item(int Times){
+            this.Times = Times;
+    }
+    }
+}
