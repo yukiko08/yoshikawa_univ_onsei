53d3e73c9a45b5d8cae7cc360d277bf178a7ee37
S2-yoshikawa <1514388t@stu.kobe-u.ac.jp>
2018-06-12 17:10:59 +0900

    slice comp

--- SlicePlay/SlicePlay/Form1.Designer.cs
+++ SlicePlay/SlicePlay/Form1.Designer.cs
@@ -28,105 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.button4 = new System.Windows.Forms.Button();
-            this.trackBar2 = new System.Windows.Forms.TrackBar();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.toruko_10 = new System.Windows.Forms.Button();
+            this.toruko_2 = new System.Windows.Forms.Button();
+            this.toruko_p = new System.Windows.Forms.Button();
+            this.trackSlice = new System.Windows.Forms.TrackBar();
+            this.anaun_p = new System.Windows.Forms.Button();
+            this.anaun_10 = new System.Windows.Forms.Button();
+            this.anaun_2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.trackPlace = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tango_p = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackSlice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackPlace)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // toruko_10
             // 
-            this.button1.Location = new System.Drawing.Point(12, 158);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "0.1秒ごと";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.toruko_10.Location = new System.Drawing.Point(12, 158);
+            this.toruko_10.Name = "toruko_10";
+            this.toruko_10.Size = new System.Drawing.Size(75, 23);
+            this.toruko_10.TabIndex = 0;
+            this.toruko_10.Text = "0.1秒ごと";
+            this.toruko_10.UseVisualStyleBackColor = true;
+            this.toruko_10.Click += new System.EventHandler(this.toruko_10_Click);
             // 
-            // button2
+            // toruko_2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 206);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "0.5秒ごと";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.toruko_2.Location = new System.Drawing.Point(12, 206);
+            this.toruko_2.Name = "toruko_2";
+            this.toruko_2.Size = new System.Drawing.Size(75, 23);
+            this.toruko_2.TabIndex = 1;
+            this.toruko_2.Text = "0.5秒ごと";
+            this.toruko_2.UseVisualStyleBackColor = true;
+            this.toruko_2.Click += new System.EventHandler(this.toruko_2_Click);
             // 
-            // button3
+            // toruko_p
             // 
-            this.button3.Location = new System.Drawing.Point(12, 35);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(104, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "調整再生";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.toruko_p.Location = new System.Drawing.Point(12, 35);
+            this.toruko_p.Name = "toruko_p";
+            this.toruko_p.Size = new System.Drawing.Size(104, 23);
+            this.toruko_p.TabIndex = 2;
+            this.toruko_p.Text = "調整再生";
+            this.toruko_p.UseVisualStyleBackColor = true;
+            this.toruko_p.Click += new System.EventHandler(this.toruko_p_Click);
             // 
-            // trackBar1
+            // trackSlice
             // 
-            this.trackBar1.Location = new System.Drawing.Point(12, 78);
-            this.trackBar1.Maximum = 1000;
-            this.trackBar1.Minimum = 10;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(104, 45);
-            this.trackBar1.SmallChange = 10;
-            this.trackBar1.TabIndex = 3;
-            this.trackBar1.Value = 10;
-            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackSlice.Location = new System.Drawing.Point(12, 78);
+            this.trackSlice.Maximum = 1000;
+            this.trackSlice.Minimum = 10;
+            this.trackSlice.Name = "trackSlice";
+            this.trackSlice.Size = new System.Drawing.Size(104, 45);
+            this.trackSlice.SmallChange = 10;
+            this.trackSlice.TabIndex = 3;
+            this.trackSlice.Value = 10;
+            this.trackSlice.Scroll += new System.EventHandler(this.trackSlice_Scroll);
             // 
-            // button4
+            // anaun_p
             // 
-            this.button4.Location = new System.Drawing.Point(157, 35);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(104, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "調整再生";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.anaun_p.Location = new System.Drawing.Point(157, 35);
+            this.anaun_p.Name = "anaun_p";
+            this.anaun_p.Size = new System.Drawing.Size(104, 23);
+            this.anaun_p.TabIndex = 4;
+            this.anaun_p.Text = "調整再生";
+            this.anaun_p.UseVisualStyleBackColor = true;
+            this.anaun_p.Click += new System.EventHandler(this.anaun_p_Click);
             // 
-            // trackBar2
+            // anaun_10
             // 
-            this.trackBar2.Location = new System.Drawing.Point(157, 78);
-            this.trackBar2.Maximum = 1000;
-            this.trackBar2.Minimum = 10;
-            this.trackBar2.Name = "trackBar2";
-            this.trackBar2.Size = new System.Drawing.Size(104, 45);
-            this.trackBar2.SmallChange = 10;
-            this.trackBar2.TabIndex = 5;
-            this.trackBar2.Value = 10;
-            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            this.anaun_10.Location = new System.Drawing.Point(168, 158);
+            this.anaun_10.Name = "anaun_10";
+            this.anaun_10.Size = new System.Drawing.Size(75, 23);
+            this.anaun_10.TabIndex = 6;
+            this.anaun_10.Text = "0.1秒ごと";
+            this.anaun_10.UseVisualStyleBackColor = true;
+            this.anaun_10.Click += new System.EventHandler(this.anaun_10_Click);
             // 
-            // button5
+            // anaun_2
             // 
-            this.button5.Location = new System.Drawing.Point(168, 158);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "0.1秒ごと";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(168, 206);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 7;
-            this.button6.Text = "0.5秒ごと";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.anaun_2.Location = new System.Drawing.Point(168, 206);
+            this.anaun_2.Name = "anaun_2";
+            this.anaun_2.Size = new System.Drawing.Size(75, 23);
+            this.anaun_2.TabIndex = 7;
+            this.anaun_2.Text = "0.5秒ごと";
+            this.anaun_2.UseVisualStyleBackColor = true;
+            this.anaun_2.Click += new System.EventHandler(this.anaun_2_Click);
             // 
             // label1
             // 
@@ -160,31 +153,93 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(193, 126);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 12);
+            this.label4.Size = new System.Drawing.Size(20, 12);
             this.label4.TabIndex = 11;
-            this.label4.Text = "10m/s";
+            this.label4.Text = "0m";
+            // 
+            // trackPlace
+            // 
+            this.trackPlace.Location = new System.Drawing.Point(157, 80);
+            this.trackPlace.Maximum = 1000;
+            this.trackPlace.Minimum = -1000;
+            this.trackPlace.Name = "trackPlace";
+            this.trackPlace.Size = new System.Drawing.Size(104, 45);
+            this.trackPlace.SmallChange = 10;
+            this.trackPlace.TabIndex = 5;
+            this.trackPlace.Scroll += new System.EventHandler(this.trackPlace_Scroll);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "切り取り幅";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(178, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "立ち位置";
+            // 
+            // tango_p
+            // 
+            this.tango_p.Location = new System.Drawing.Point(302, 35);
+            this.tango_p.Name = "tango_p";
+            this.tango_p.Size = new System.Drawing.Size(75, 23);
+            this.tango_p.TabIndex = 14;
+            this.tango_p.Text = "調節再生";
+            this.tango_p.UseVisualStyleBackColor = true;
+            this.tango_p.Click += new System.EventHandler(this.tango_p_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(300, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "アナウンサー単語";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(289, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 88);
+            this.listBox1.TabIndex = 16;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(410, 261);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tango_p);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.trackBar2);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.anaun_2);
+            this.Controls.Add(this.anaun_10);
+            this.Controls.Add(this.trackPlace);
+            this.Controls.Add(this.anaun_p);
+            this.Controls.Add(this.trackSlice);
+            this.Controls.Add(this.toruko_p);
+            this.Controls.Add(this.toruko_2);
+            this.Controls.Add(this.toruko_10);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackSlice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackPlace)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -192,18 +247,23 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TrackBar trackBar2;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button toruko_10;
+        private System.Windows.Forms.Button toruko_2;
+        private System.Windows.Forms.Button toruko_p;
+        private System.Windows.Forms.TrackBar trackSlice;
+        private System.Windows.Forms.Button anaun_p;
+        private System.Windows.Forms.Button anaun_10;
+        private System.Windows.Forms.Button anaun_2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackPlace;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button tango_p;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- SlicePlay/SlicePlay/Form1.cs
+++ SlicePlay/SlicePlay/Form1.cs
@@ -8,23 +8,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.DirectX.AudioVideoPlayback;
+using System.Threading;
 
 namespace SlicePlay
 {
     public partial class Form1 : Form
     {
-        Audio toruko = new Audio(@"C:\Users\S2\Documents\yoshikawa_univ_onsei\ffmpeg_static\input_t.wav");
-        Audio anaun = new Audio(@"C:\Users\S2\Documents\yoshikawa_univ_onsei\ffmpeg_static\input_a.wav");
+        Audio toruko_r = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\input_t.wav");
+        Audio toruko_l = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\input_t.wav");
+        Audio anaun_r = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\input_a.wav");
+        Audio anaun_l = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\input_a.wav");
+        Audio beep = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\p2000.wav");
+        
+
+        string[] file = System.IO.Directory.GetFiles(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\tango_demo", "*", System.IO.SearchOption.AllDirectories);
+     
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void toruko_10_Click(object sender, EventArgs e)
         {
-            Audio right = new Audio(@"C:\Users\S2\Documents\yoshikawa_univ_onsei\ffmpeg_static\bin\output_10r.wav");
-            Audio left = new Audio(@"C:\Users\S2\Documents\yoshikawa_univ_onsei\ffmpeg_static\bin\output_10l.wav");
+            Audio right = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\output_10r.wav");
+            Audio left = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\output_10l.wav");
 
             right.Balance = 10000;
             left.Balance = -10000;
@@ -32,37 +40,10 @@ namespace SlicePlay
             right.Play();
             left.Play();
         }
-        private void button5_Click(object sender, EventArgs e)
+        private void anaun_10_Click(object sender, EventArgs e)
         {
-            Audio right = new Audio(@"C:\Users\S2\Documents\yoshikawa_univ_onsei\ffmpeg_static\bin\output_10r_a.wav");
-            Audio left = new Audio(@"C:\Users\S2\Documents\yoshikawa_univ_onsei\ffmpeg_static\bin\output_10l_a.wav");
-
-            right.Balance = 10000;
-            left.Balance = -10000;
-
-            right.Play();
-            System.Threading.Thread.Sleep(50);
-            left.Play();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Audio right = new Audio(@"C:\Users\S2\Documents\yoshikawa_univ_onsei\ffmpeg_static\bin\output_2r.wav");
-            Audio left = new Audio(@"C:\Users\S2\Documents\yoshikawa_univ_onsei\ffmpeg_static\bin\output_2l.wav");
-
-            right.Balance = 10000;
-            left.Balance = -10000;
-
-            right.Play();
-
-            System.Threading.Thread.Sleep(50);
-            left.Play();
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            Audio right = new Audio(@"C:\Users\S2\Documents\yoshikawa_univ_onsei\ffmpeg_static\bin\output_2r_a.wav");
-            Audio left = new Audio(@"C:\Users\S2\Documents\yoshikawa_univ_onsei\ffmpeg_static\bin\output_2l_a.wav");
+            Audio right = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\output_10r_a.wav");
+            Audio left = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\output_10l_a.wav");
 
             right.Balance = 10000;
             left.Balance = -10000;
@@ -72,49 +53,142 @@ namespace SlicePlay
             left.Play();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void toruko_2_Click(object sender, EventArgs e)
         {
-            toruko.Play();
-            play_t();
-        }
-        private void button4_Click(object sender, EventArgs e)
-        {
-            anaun.Play();
-            play_a();
+            Audio right = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\output_2r.wav");
+            Audio left = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\output_2l.wav");
+
+            right.Balance = 10000;
+            left.Balance = -10000;
+
+            right.Play();
+
+            System.Threading.Thread.Sleep(50);
+            left.Play();
         }
 
-        private async void play_t()
+        private void anaun_2_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < 100; i++)
+            Audio right = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\output_2r_a.wav");
+            Audio left = new Audio(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\output_2l_a.wav");
+
+            right.Balance = 10000;
+            left.Balance = -10000;
+
+            right.Play();
+            System.Threading.Thread.Sleep(50);
+            left.Play();
+        }
+
+        private void toruko_p_Click(object sender, EventArgs e)
+        {
+            toruko_r.Stop();
+            toruko_l.Stop();
+
+            toruko_r.Balance = 10000;
+            toruko_l.Balance = -10000;
+            if (trackPlace.Value > 0)
             {
-                toruko.Balance=10000;
-                await Task.Delay(trackBar1.Value);
-                toruko.Balance = -10000;
-                await Task.Delay(trackBar1.Value);
+                toruko_r.Play();
+                kiritori(toruko_r, 0);
+                Thread.Sleep(trackPlace.Value * 3);
+                toruko_l.Play();
+                kiritori(toruko_l, trackSlice.Value);
+            }
+            else
+            {
+                toruko_l.Play();
+                kiritori(toruko_l, 0);
+                Thread.Sleep(Math.Abs(trackPlace.Value) * 3);
+                toruko_r.Play();
+                kiritori(toruko_r, trackSlice.Value);
+            }
+            
+        }
+        private void anaun_p_Click(object sender, EventArgs e)
+        {
+            anaun_r.Stop();
+            anaun_l.Stop();
+
+            anaun_r.Balance = 10000;
+            anaun_l.Balance = -10000;
+
+            if (trackPlace.Value > 0)
+            {
+                anaun_r.Play();
+                kiritori(anaun_r, 0);
+                Thread.Sleep(trackPlace.Value * 3);
+                anaun_l.Play();
+                kiritori(anaun_l, trackSlice.Value);
+            }
+            else
+            {
+                anaun_l.Play();
+                kiritori(anaun_l, 0);
+                Thread.Sleep(Math.Abs(trackPlace.Value) * 3);
+                anaun_r.Play();
+                kiritori(anaun_r,  trackSlice.Value);
+            }
+            
+        }
+
+        private async void kiritori(Audio onsei,int zure)
+        {
+            await Task.Delay(zure);
+
+            for (int i = 0; i < 300; i++)
+            {
+                onsei.Volume = 0;
+                await Task.Delay(trackSlice.Value);
+                onsei.Volume = -10000;
+                await Task.Delay(trackSlice.Value);
             }
         }
-        private async void play_a()
+
+       
+
+        private void trackSlice_Scroll(object sender, EventArgs e)
         {
-            for (int i = 0; i < 100; i++)
+            label3.Text = trackSlice.Value + "m/s";
+        }
+
+        private void trackPlace_Scroll(object sender, EventArgs e)
+        {
+            label4.Text = trackPlace.Value + "m";
+        }
+
+
+        private void tango_p_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.AddRange(file);
+            Random r = new System.Random();
+            beep.Volume = -500;
+
+            for (int i = 0; i < file.Length; i+=2)
             {
-                anaun.Balance = 10000;
-                await Task.Delay(trackBar2.Value);
-                anaun.Balance = -10000;
-                await Task.Delay(trackBar2.Value);
+                Audio tango = new Audio(file[i]);
+                Audio tango_n = new Audio(file[i + 1]);
+                tango.Play();
+                Thread.Sleep(Convert.ToInt32(tango.Duration*1000+tango_n.Duration*1000));
+                /*
+                int j=i;
+                while(i==j){
+                   j = r.Next(10);
+                }
+                Audio zatuon = new Audio(file[j]);
+
+                beep.Play();
+                zatuon.Play();
+                Thread.Sleep(Convert.ToInt32(zatuon.Duration * 1000));
+                beep.Stop();*/
+                
             }
+
+
         }
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
-            label3.Text = trackBar1.Value + "m/s";
-        }
 
-        private void trackBar2_Scroll(object sender, EventArgs e)
-        {
-            label4.Text = trackBar2.Value + "m/s";
-        }
 
-        
 
         
 
