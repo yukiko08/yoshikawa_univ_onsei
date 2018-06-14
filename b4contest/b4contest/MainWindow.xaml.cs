b3c4de6e6e0c9a85e51a1f517a33f07935033137
S2-yoshikawa <1514388t@stu.kobe-u.ac.jp>
2018-06-15 00:20:41 +0900

    class

--- b4contest/b4contest/MainWindow.xaml.cs
+++ b4contest/b4contest/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WindowsFormsApplication1;
+using Microsoft.DirectX.AudioVideoPlayback;
 
 namespace b4contest
 {
@@ -20,39 +22,44 @@ namespace b4contest
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<object> boin = new List<object>();
+        List<object> shin = new List<object>();
+
         public MainWindow()
         {
             InitializeComponent();
 
-            CmbObject none =new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\none.wav", "×");
 
-            List<object> boin = new List<object>();
+            boin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\none.wav", "×"));
+                                                           
+            boin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\a.wav", "a"));
+            boin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\i.wav", "i"));
+            boin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\u.wav", "u"));
+            boin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\e.wav", "e"));
+            boin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\o.wav", "o"));
+                                                            
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\none.wav", "×"));
+                                                            
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\k.wav", "k"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\s.wav", "s"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\t.wav", "t"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\n.wav", "n"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\h.wav", "h"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\m.wav", "m"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\j.wav", "y"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\r.wav", "r"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\w.wav", "w"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\g.wav", "g"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\z.wav", "z"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\d.wav", "d"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\b.wav", "b"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\p.wav", "p"));
+            shin.Add(new CmbObject("C:\\Users\\S2\\OneDrive\\デスクトップ\\研究室\\子音と母音\\v.wav", "v"));
 
-            boin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\a.wav", "a"));
-            
-            boin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\i.wav", "i"));
-            boin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\u.wav", "u"));
-            boin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\e.wav", "e"));
-            boin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\o.wav", "o"));
-            
-            List<object> shin = new List<object>();
-
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\k.wav", "k"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\s.wav", "s"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\t.wav", "t"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\n.wav", "n"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\h.wav", "h"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\m.wav", "m"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\j.wav", "y"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\r.wav", "r"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\w.wav", "w"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\g.wav", "g"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\z.wav", "z"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\d.wav", "d"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\b.wav", "b"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\p.wav", "p"));
-            shin.Add(new CmbObject("C:\\Users\\S2\\Music\\日本語発音\\wav\\v.wav", "v"));
-            
+            Audio sound_b = new Audio(boin[0].ToString());
+            sound_b.Balance = 10000;
+            Audio sound_s = new Audio(shin[0].ToString());
+            sound_s.Balance = -10000;
              
         }
 
@@ -68,9 +75,67 @@ namespace b4contest
             }
             public override string ToString()
             {
-                return Value;
+                return Url;
             }
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<List<string>> data = new List<List<string>>();
+
+            data = null;
+
+
+            using (var csv = new CsvReader(@"C:\Users\S2\OneDrive\デスクトップ\研究室\子音と母音\result_time.csv"))
+            {
+                data = csv.ReadToEnd();
+            }
+
+            //配列の作成
+            string text = textBox.Text;
+            string[] str = new string[text.Length];
+
+            string[] url_s = new string[text.Length];
+            string[] url_b = new string[text.Length];
+
+            int[] times = new int[text.Length];
+
+            for (int i = 0; i < text.Length; i++) str[i] = text.Substring(i, 1);
+
+            //検索して格納
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int x = 0;
+                while (x < data.Count)
+                {
+                    if (str[i] == data[x][0])
+                    {
+                        object s =shin[shin.IndexOf(data[x][1])];
+                        url_s[i] = s.ToString();
+                        object b = shin[shin.IndexOf(data[x][1])];
+                        url_b[i] = b.ToString();
+                        times[i] = Int32.Parse(data[x][3]);
+
+                        break;
+                    }
+                    else
+                    {
+                        x++;
+                    }
+
+                }
+            }
+
+        }
+        private async void shin_play(string[] urls)
+        {
+            for (int i = 0; i < urls.Length; i++)
+            {
+
+            }
+        }
     }
+     
 }
